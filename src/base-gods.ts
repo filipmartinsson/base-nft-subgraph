98a317f26c66b6be01b03ce8800cee4800d6bc39
Filip Martinsson <martinsson.filip@gmail.com>
2024-03-21 10:27:12 +0100

    final version

--- src/base-gods.ts
+++ src/base-gods.ts
@@ -24,8 +24,17 @@ import {
   ProvenanceHashUpdated,
   RoyaltyInfoUpdated,
   SeaDropTokenDeployed,
-  Transfer
+  Transfer,
+  Token,
+  TokenMetadata,
+  User
 } from "../generated/schema"
+
+import { TokenMetadata as TokenMetadataTemplate } from "../generated/templates";
+import { json, Bytes, dataSource } from "@graphprotocol/graph-ts";
+
+
+const ipfsHash = "bafybeicjp6ph5coohgmwjtehxrbptmeklxqhhtqobz447ma3qcvmyy6lwi"
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
@@ -205,4 +214,44 @@ export function handleTransfer(event: TransferEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let token = Token.load(event.params.tokenId.toString())
+  if (!token) {
+    token = new Token(event.params.tokenId.toString())
+    token.tokenID = event.params.tokenId
+    
+    token.tokenURI = "/" + event.params.tokenId.toString();
+    const ipfsHashUri = ipfsHash + token.tokenURI;
+    token.ipfsHashURI = ipfsHashUri;
+   
+    TokenMetadataTemplate.create(ipfsHashUri)
+  }
+  token.updatedAtTimestamp = event.block.timestamp
+  token.owner = event.params.to.toHexString()
+  token.save()
+
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+      user = new User(event.params.to.toHexString());
+      user.save();
+  }
+}
+export function handleMetadata(content: Bytes): void {
+  let tokenMetadata = new TokenMetadata(dataSource.stringParam());
+  // Create a new TokenMetadata entity and pass in the dataSource as its ID. This is the ipfsHashUri that we created in the handleTransfer function above.
+
+  const value = json.fromBytes(content).toObject();
+  // Create a value variable that will be used to store the json object that is passed in as the content parameter.
+  if (value) {
+      const image = value.get("image");
+      const name = value.get("name");
+      // Assemblyscript needs to have nullchecks. If the value exists, then we can proceed with the creating an image, name, and attributes variable gathered from the json object.
+
+      if (name && image) {
+          tokenMetadata.metadata = content.toString()
+          tokenMetadata.name = name.toString();
+          tokenMetadata.image = image.toString();
+          tokenMetadata.save();
+      }
+  }
 }
